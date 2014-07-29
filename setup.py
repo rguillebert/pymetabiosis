42ef2279279ec849c8911735a04e74a564111845
Romain Guillebert <romain.py@gmail.com>
2014-07-29 14:30:49 +0200

    Add boilerplate

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pymetabiosis",
+    version="0.0.1",
+
+    author="Romain Guillebert",
+    author_email="romain.py@gmail.com",
+
+    packages=find_packages(),
+    license="MIT",
+    description="A way of using CPython 2's modules on other cffi compatible Python implementations"
+)
