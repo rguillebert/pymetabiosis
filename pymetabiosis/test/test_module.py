f43d645b68614c7eb805085ed1fc697d0ec41257
Romain Guillebert <romain.py@gmail.com>
2014-08-12 18:01:57 +0200

    Add the first test

---
+++ pymetabiosis/test/test_module.py
@@ -0,0 +1,7 @@
+import os.path
+from pymetabiosis import import_module
+
+def test_import_sqlite():
+    module = import_module("sqlite")
+    module_str = str(module)
+    assert module_str.startswith("<module 'sqlite' from ")
